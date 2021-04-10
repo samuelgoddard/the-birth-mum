7d23c355625aef86ecdfa572e13d979e0a36c57a
Tom Nightingale <tom@weareleague.co.uk>
2021-04-10 17:05:00 +0100

    fix about excerpt

--- components/about-excerpt.js
+++ components/about-excerpt.js
@@ -8,7 +8,6 @@ export default function AboutExcerpt({ heading, intro }) {
             <p className="pr-4 mb-4 text-3xl leading-snug md:w-1/2 font-display xs:text-4xl sm:text-5xl xl:text-6xl 2xl:text-7xl 2xl:leading-tight">{heading}</p>
 
             <div className="md:w-1/2">
-                <p className="text-lg leading-relaxed opacity-75 md:w-4/5 2xl:pl-20">The Birth Mum classes are based in Nottingham and the surrounding Nottinghamshire areas. You can choose to attend a group course or a private course, it’s completely up to you and your preferences. Get in touch today to find out more, book a course or have a chat about your options.</p>
                 <div className="text-lg leading-relaxed opacity-75 md:w-4/5 2xl:pl-20" dangerouslySetInnerHTML={{ __html:intro }} />
             </div>
 
