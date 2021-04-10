c5482d1b784452bd5d6a5e905bc1ba3bc5e6fbb2
Tom Nightingale <tom@weareleague.co.uk>
2021-04-10 17:06:40 +0100

    fix about excerpt

--- components/about-excerpt.js
+++ components/about-excerpt.js
@@ -3,7 +3,7 @@ import Container from '../components/container'
 export default function AboutExcerpt({ heading, intro }) {
     return(
         <Container>
-            <div className="relative z-10 flex flex-wrap items-center mt-20">
+            <div className="relative z-10 flex flex-wrap items-center my-20">
 
             <p className="pr-4 mb-4 text-3xl leading-snug md:w-1/2 font-display xs:text-4xl sm:text-5xl xl:text-6xl 2xl:text-7xl 2xl:leading-tight">{heading}</p>
 
